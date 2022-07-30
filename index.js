dbd65ec2f333be0c10e6b42d243176dfc50ca6f1
Gautam Kumar <79142679+Gautam-012@users.noreply.github.com>
2022-07-30 18:30:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function alertModal(){
+    swal({
+      title: "Enquiry Received!",
+      text: "We will reply you very soon!",
+      icon: "success",
+      button: "Continue!",
+    });
+  }
+  
+  function sendEmail(){
+    alertModal();
+};
